ecdb9d2acaf5753d113362d652b12b6719d092ec
Colin Fahy <colinfahy@spotify.com>
2021-03-12 23:09:06 -0500

    formatting and tests

--- {{cookiecutter.project_slug}}/tests/test_cli.py
+++ {{cookiecutter.project_slug}}/tests/test_cli.py
@@ -18,5 +18,15 @@ def runner():
 
 
 def test_cli(runner):
-    result = runner.invoke(cli.cli, ['--verbose'])
+    result = runner.invoke(cli.cli, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_cli_train(runner):
+    result = runner.invoke(cli.cli, ["-v", "train"])
+    assert result.exit_code == 0
+
+
+def test_cli_predict(runner):
+    result = runner.invoke(cli.cli, ["-v", "predict"])
     assert result.exit_code == 0
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -1,6 +1,10 @@
 """{{ cookiecutter.project_short_description }}"""
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def main():
     """This is all my project does"""
-    return 'hello'
+    logger.info("No-op")
+    return "hello"
