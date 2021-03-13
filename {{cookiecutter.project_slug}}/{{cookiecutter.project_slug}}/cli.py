af437a3f0d7d1bef79781f1fdf10d70019d479a7
Colin Fahy <colinfahy@spotify.com>
2021-03-12 21:27:56 -0500

    modernize poetry+tox interactions

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -1,15 +1,40 @@
 """Command line interface for {{ cookiecutter.project_slug }}"""
 import click
+import logging
 
 from {{ cookiecutter.project_slug }} import main
 
 
-@click.command()
-@click.option('-v', '--verbose', count=True)
-@click.option('--option', default='default_val', help='An example optional parameter')
-def cli(option, verbose):
+@click.group()
+@click.option("-v", "--verbose", count=True)
+@click.pass_context
+def cli(ctx, verbose):
+    ctx.show_default = True
+    initLogging(verbose)
+
+
+@cli.command()
+def train():
     click.echo(click.style('Hello {{ cookiecutter.project_slug }}!', fg='magenta', bold=True))
-    click.echo('Verbosity level: {}'.format(verbose))
-    click.echo('Optional param: {}'.format(option))
     result = main.main()
     click.echo('Main output: {}'.format(result))
+
+
+@cli.command()
+def predict():
+    click.echo(click.style('Hello {{ cookiecutter.project_slug }}!', fg='magenta', bold=True))
+    result = main.main()
+    click.echo('Main output: {}'.format(result))
+
+
+def initLogging(verbosity=0):
+    """Setup logging with a given verbosity level"""
+    # import logging.config
+    # logging.config.fileConfig('logging_config.ini', disable_existing_loggers=False)
+    logging.basicConfig()
+    if verbosity == 0:
+        logging.root.setLevel(logging.WARN)
+    if verbosity == 1:
+        logging.root.setLevel(logging.INFO)
+    if verbosity > 1:
+        logging.root.setLevel(logging.DEBUG)
